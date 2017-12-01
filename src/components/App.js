a83a9dfcbb7e0391be978fb298e53eea5fde6a33
Peter <peter.prentiss@gmail.com>
2017-11-30 22:27:09 -0600

    Create Header component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from './Header';
+
 export default class App extends Component {
     render() {
         return (
-            <div>React/Redux simple starter</div>
+            <div>
+                <Header />
+            </div>
         );
     }
 }
