5bed47d8e07c31a507e30a83f0c14334fe8e25b1
Peter <peter.prentiss@gmail.com>
2017-12-01 00:38:03 -0600

    Fixed react router issues

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Home from './Home';
+import Resources from './Resources';
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <Header />
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/resources" component={Resources} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
         );
     }
 }
